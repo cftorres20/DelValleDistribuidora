e2a74566727d3d1606f0287bdb68b7d5dd06df5c
cftorres20 <cftorres20@gmail.com>
2024-11-19 20:03:50 -0300

    se renderiza los productos en promo de la pag tienda

--- assets/js/main-tienda.js
+++ assets/js/main-tienda.js
@@ -1,4 +1,12 @@
 import { titulosTienda } from "./titulos.js";
-import { tituloDinamico } from "./funciones.js";
+import { tituloDinamico, renderizarPromocionesInicio } from "./funciones.js";
+import { arrayProductos } from "./catalogo.js";
 
+//---Titulo
 tituloDinamico(titulosTienda);
+
+//---RENDERIZADO DE LAS PROMOCIONES
+const contenedorPromociones = document.getElementById("promociones-contenedor");
+const arregloProductos = arrayProductos.productos;
+
+renderizarPromocionesInicio(arregloProductos, contenedorPromociones);
